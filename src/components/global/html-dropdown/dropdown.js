9fe8b40d89f31b4377371ecff11351daff2f3cec
Blake <contact@blakezajac.com>
2022-12-15 14:46:33 +1100

    Update html-dropdown dropdown.js

--- src/components/global/html-dropdown/dropdown.js
+++ src/components/global/html-dropdown/dropdown.js
@@ -1,28 +1,34 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    $(".dropdown__field").each(function (index) {
-        window.onclick = function (event) {
-            console.log(event.target);
-            if (!event.target.matches(".dropdown")) {
-                if ($(".dropdown__items__list").hasClass("open")) {
-                    $(".dropdown__items__list").removeClass("open");
+    function dropdownMenu() {
+        $(".dropdown__field").each(function (index) {
+            window.onclick = function (event) {
+                console.log(event.target);
+                if (!event.target.matches(".dropdown")) {
+                    if ($(".dropdown__items__list").hasClass("open")) {
+                        $(".dropdown__items__list").removeClass("open");
+                    }
                 }
-            }
-        };
-        $(this).on("click", function (event) {
-            var hc = $(this)
-                .parent()
-                .find(".dropdown__items__list")
-                .hasClass("open");
-            console.log($(this).parent().find(".dropdown__items__list"));
-            console.log(hc);
-            $(".dropdown__items__list").removeClass("open");
-            if (!hc) {
-                $(this)
+            };
+            $(this).on("click", function (event) {
+                var hc = $(this)
                     .parent()
                     .find(".dropdown__items__list")
-                    .addClass("open");
-            }
-            event.stopPropagation();
+                    .hasClass("open");
+                console.log($(this).parent().find(".dropdown__items__list"));
+                console.log(hc);
+                $(".dropdown__items__list").removeClass("open");
+                if (!hc) {
+                    $(this)
+                        .parent()
+                        .find(".dropdown__items__list")
+                        .addClass("open");
+                }
+                event.stopPropagation();
+            });
         });
-    });
+    }
+
+    if (dropdownMenu) {
+        dropdownMenu();
+    }
 });
