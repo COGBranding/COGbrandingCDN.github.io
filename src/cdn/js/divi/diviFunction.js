570294864ead8fde9693d7ed6365a3704dee364a
Blake <contact@blakezajac.com>
2022-12-16 17:29:32 +1100

    Added dropdownMenu function

--- src/cdn/js/divi/diviFunction.js
+++ src/cdn/js/divi/diviFunction.js
@@ -616,3 +616,34 @@ function gooeyCursor() {
         updateCursor();
     }
 }
+
+function dropdownMenu() {
+    if ($(".dropdown__field").length) {
+        $(".dropdown__field").each(function (index) {
+            window.onclick = function (event) {
+                console.log(event.target);
+                if (!event.target.matches(".dropdown")) {
+                    if ($(".dropdown__items__list").hasClass("open")) {
+                        $(".dropdown__items__list").removeClass("open");
+                    }
+                }
+            };
+            $(this).on("click", function (event) {
+                var hc = $(this)
+                    .parent()
+                    .find(".dropdown__items__list")
+                    .hasClass("open");
+                console.log($(this).parent().find(".dropdown__items__list"));
+                console.log(hc);
+                $(".dropdown__items__list").removeClass("open");
+                if (!hc) {
+                    $(this)
+                        .parent()
+                        .find(".dropdown__items__list")
+                        .addClass("open");
+                }
+                event.stopPropagation();
+            });
+        });
+    }
+}
