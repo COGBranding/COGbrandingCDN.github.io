ac998ef1d20986191cf1c775980eaa47ea4d28c3
Blake <contact@blakezajac.com>
2023-02-24 13:18:44 +1100

    Add hideEmptyParagraphs() function

--- src/cdn/js/divi/diviFunction.js
+++ src/cdn/js/divi/diviFunction.js
@@ -756,3 +756,13 @@ function createSpanText(selector) {
             .join("");
     }
 }
+
+function hideEmptyParagraphs(selector) {
+    const paragraphs = document.querySelectorAll(selector);
+
+    paragraphs.forEach((paragraph) => {
+        if (!paragraph.textContent.trim()) {
+            paragraph.style.display = "none";
+        }
+    });
+}
